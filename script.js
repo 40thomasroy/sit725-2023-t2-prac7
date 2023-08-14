89663719b3e4cb544cbaf2cffcc7ba1d82b919ad
40thomasroy <40thomasroy@gmail.com>
2023-08-14 16:24:45 +1000

    finished

--- script.js
+++ script.js
@@ -1,45 +1,54 @@
-const cardList = [{
-    title: 'cat 2',
-    path: 'images/cat1.jpg',
-    subTitle: 'About Cat 2',
-    description: 'Description of Cat 2'
-},
-{
-    title: 'cat 3',
-    path: 'images/cat2.jpg',
-    subTitle: 'About Cat 3',
-    description: 'Description of Cat 3'
-},
-];
-
 const addCards = (items) => {
     items.forEach(item => {
-        let itemToAppend = `<div class="col s4 center-align">
-            <div class="card medium"><div class="card-image waves-effect waves-block waves-light"><img class="activator" src="${item.path}">
-            </div><div class="card-content">
-            <span class="card-title activator grey-text text-darken-4">${item.title}<i class="material-icons right">more_vert</i></span><p><a href="#">${item.link}</a></p></div>
-            <div class="card-reveal">
-            <span class="card-title grey-text text-darken-4">${item.title}<i class="material-icons right">close</i></span>
-            <p class="card-text">${item.description}</p>
-            </div></div></div>`;
-        $("#card-section").append(itemToAppend);
+        let itemToAppend = '<div class="col s4 center-align">' +
+            '<div class="card medium"><div class="card-image waves-effect waves-block waves-light"><img class="activator" src="' + item.path + '">' +
+            '</div><div class="card-content">' +
+            '<span class="card-title activator grey-text text-darken-4">' + item.title + '<i class="material-icons right">more_vert</i></span><p><a href="#">' + item.subTitle + '</a></p></div>' +
+            '<div class="card-reveal">' +
+            '<span class="card-title grey-text text-darken-4">' + item.subTitle + '<i class="material-icons right">close</i></span>' +
+            '<p class="card-text">' + item.description + '</p>' +
+            '</div></div></div>';
+        $("#card-section").append(itemToAppend)
     });
-};
-const formSumitted = () => {
-    let formData = {};
-    formData.firstName = $('#first_name').val();
-    formData.lastName = $('#last_name').val();
-    formData.password = $('#password').val();
-    formData.email = $('#email').val();
-
-    console.log(formData);
 }
 
-$(document).ready(function(){
+const formSumitted = () => {
+    let formData = {};
+    formData.title = $('#title').val();
+    formData.subTitle = $('#subTitle').val();
+    formData.path = $('#path').val();
+    formData.description = $('#description').val();
+
+    console.log(formData);
+    postCat(formData);
+}
+
+function postCat(cat) {
+    $.ajax({
+        url: '/api/cat',
+        type: 'POST',
+        data: cat,
+        success: (result) => {
+            if (result.statusCode === 201) {
+                alert('cat added');
+            }
+        }
+    });
+}
+
+function getAllCats() {
+    $.get('/api/cats', (result) => {
+        if (result.statusCode === 200) {
+            addCards(result.data);
+        }
+    });
+}
+
+$(document).ready(function () {
     $('.materialboxed').materialbox();
-    $('#formSubmit').click(()=>{
+    $('#formSubmit').click(() => {
         formSumitted();
     });
-    addCards(cardList);
     $('.modal').modal();
+    getAllCats();
 });
--- server.js
+++ server.js
@@ -1,23 +1,72 @@
 const express = require('express');
-let app = express();
-let port = process.env.PORT || 3000;
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const app = express();
+const port = 3000;
+
 app.use(express.static(__dirname + '/'));
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + 'index.html');
-});
-app.get('/addTwoNumbers', (req, res) => {
-    let cd = 200;
-    let msg = 'success'
-    let num1 = parseInt(req.query.num1);
-    let num2 = parseInt(req.query.num2);
-    let sum = (num1) + (num2);
-    res.json({
-        Status: msg,
-        code: cd,
-        data: sum
-    })
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+const uri = "mongodb+srv://40thomasroy:40Thomasroy@cluster0.klekhm5.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
 });
 
-app.listen(port, () => {
+let collection;
+
+async function runDB() {
+    try {
+        await client.connect();
+        await client.db("admin").command({ ping: 1 });
+
+        collection = client.db().collection('Cats');
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } catch (ex) {
+        console.error(ex);
+    }
+}
+
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + '/index.html');
+});
+
+
+
+
+app.post('/api/cat', (req,res)=>{
+    let cat = req.body;
+    console.log(cat);
+    insertCat(cat, (err,result) => {
+        if(!err){
+            res.json({statusCode:201,data:result,message:'success'});
+        }
+    });
+});
+
+app.get('/api/cats', (req,res)=>{
+    getAllCats((err,result)=>{
+        console.log(result);
+        if(!err){
+            res.json({statusCode:200,data:result,message:'success'});
+        }
+    });
+});
+
+function insertCat(cat, callback) {
+    collection.insertOne(cat, callback);
+}
+
+function getAllCats(callback) {
+    collection.find({}).toArray(callback);
+}
+
+runDB().catch(console.dir);
+
+app.listen(port, async () => {
     console.log(`Server started on port ${port}`);
+    await runDB();
 });
